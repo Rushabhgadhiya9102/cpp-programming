39c9e5eee485f69c2bdb50214ddfe0e6ea5b502e
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-12-04 18:01:24 +0530

    done

--- extra-wrk/class_and_object.cpp
+++ extra-wrk/class_and_object.cpp
@@ -1,37 +1,27 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
 
-class Student{              // DECLARE THE CLASS 
-     public:                // ACCESS SPECIFIER = PUBLIC
-        int grId;
-        char name[100];
-        int age;
-        char courseName[100];
+class Employee {             // DECLARE THE CLASS
 
-    };
+  private:
+    int salary;             // ACCESS SPECIFIER = PUBLIC
 
-int main(){
+  public:
+    void setSalary(int s) {
+        salary = s;
+    }
 
-    Student s1;     // DECLARE THE OBJECT
+    int getSalary() {
+        return salary;
+    }
+};
 
-    // ASSIGN THE VALUE
+int main() {
 
-    s1.grId = 7348;
-    strcpy(s1.name,"Rushabh_gadhiya");
-    s1.age = 22;
-    strcpy(s1.courseName, "full-stack-development");
+    Employee myObj;
 
-    // ASSIGN THE VALUE
-
-    // PRINT THE STATEMENT
-
-    cout << "Grid : " << s1.grId << endl
-         << "Name : " << s1.name << endl
-         << "Age : " << s1.age << endl
-         << "Coursename : " << s1.courseName;  
-         
-    // PRINT THE STATEMENT           
+    myObj.getSalary(50000);
+    cout << myObj.getSalary();
 
     return 0;
 }
