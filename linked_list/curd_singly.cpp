4d290f01d7eeffd4ec48096831cb5704232b67bc
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-12-30 18:17:43 +0530

    done

---
+++ linked_list/curd_singly.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data:
+        Node *next;
+
+        Node (int data) {
+            this -> data = data;
+            this -> next = NULL;
+        }
+};
+
+class LinkedList{
+    public:
+        Node *head;
+        int count;
+
+        LinkedList(){
+            this -> head = NULL;
+            count = 0;
+        }
+
+        void addAtBeginning(int data){
+            Node *newNode = new Node(data);
+            newNode ->next = this->head;
+            this -> head = newNode;
+            this -> count++;
+        }
+
+        void addAtEnd(int data){
+            Node *newNode = new Node(data);
+            if(this->head == NULL){
+                this->head = newNode;
+            }else{
+                Node *ptr = this -> head;
+                while(ptr->next!=NULL){
+                    ptr = ptr -> next;
+                }
+                ptr -> next = newNode;
+            }
+            this->count++;
+        }
+
+        void addAtPosition(int data, int position){
+            Node *newNode = new Node(data);
+
+            if(position == 0){
+                newNode -> next = this->head;
+                this->head = newNode;
+            }else{
+                Node *ptr = head;
+            }
+        }
+};
